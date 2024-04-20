f5bc94a739f4565c8260c4e056e6a42a07be85e6
DESKTOP-AV40OH0\Cardin <cardinhschool@gmail.com>
2024-04-20 02:21:04 -0400

    no message

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+import logo from "../assets/logo.png";
+
+ 
+const Home = () => {
+    return (
+        <div>
+            <img src={logo} alt="logo" />
+            <h1>TravelWise</h1>
+
+        </div>
+    );
+};
+ 
+export default Home;
